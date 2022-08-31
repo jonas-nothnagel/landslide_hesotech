0b35ff5352f8c82bb7e27b8596ea51335f51f3f9
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-08-31 09:28:46 +0200

    add keys

---
+++ langenthal/main.py
@@ -0,0 +1,29 @@
+#%%
+import requests
+from pathlib import Path
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'src')))
+#%%
+clientCrt = "../certificates/pilot-users.crt"
+clientKey = "../certificates/pilot-users.key"
+#%%
+url = "https://langenthal.hesotech.eu/imaster/api/v1/TestRunReportingService/GetMeasurementSetList"
+
+headers = {'content-type': 'application/json'}
+payload = {
+  'chartQuery': {
+    'chartId': '02a96b64-7792-a940-4fe5-edace24deeb0',
+    'from': '2021-09-17T09:29:54.252178+00:00',
+    'till': '',
+    'maxNumberOfPoints': 1
+  }
+}
+r = requests.post(url, headers=headers, cert=(clientCrt, clientKey), json=payload)
+
+print(r.status_code)
+print(r.json())
+
+
+
+# %%
