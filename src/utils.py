c8b92e6f3d7f5d33050a0394f6d1c54d8f65795e
jonas.nothnagel@gmail.com <jonas.nothnagel@gmail.com>
2022-09-02 13:07:03 +0200

    expanded notebooks

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import requests
+import copy
+import pandas as pd 
 import glob, os, sys
 
 
@@ -27,3 +29,19 @@ def query_image(scence, layer, column, row, timestamp = "latest"):
     r = requests.get(url, headers=headers, cert=(clientCrt, clientKey))
 
     return url, r
+
+def strip_scans(d, query):
+
+    # import json with timestamps
+    df = pd.read_json("../data/scene_0_scans.json")
+    d = df.to_dict(orient = 'records')
+
+    # copy dict
+    d_copy = copy.deepcopy(d)
+
+    query = str(query)
+    temp_val = [value for value in d_copy if query in value["ScanName"]]
+
+    values = [i['ScanName'] for i in temp_val]
+    
+    return values, d
